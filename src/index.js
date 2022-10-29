50991bdcf486cb4ff5f3248589aa8a2837347669
hithisisat <32217807+hithisisat@users.noreply.github.com>
2022-10-29 22:12:39 +0530

    updated code

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//main code starts from here
 root.render(
   <React.StrictMode>
     <App />
